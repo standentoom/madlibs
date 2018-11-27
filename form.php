d71e2a1b51ad8f7188b96be0e112010336c00f00
stan den toom <99041312@mydavinci.nl>
2018-11-27 13:30:25 +0100

    input is showed in the mood.php

---
+++ form.php
@@ -0,0 +1,25 @@
+<html>
+<head>
+  <title>Formulier</title>
+  <link href="form.css" rel="stylesheet" type="text/css" />
+</head>
+
+<body>
+  <form method="post" action="mood.php">
+    <p>
+        <label>Wat ben je aan het doen?</label>
+        <input type="text" name="activity" />
+    </p>
+    
+    <p>
+        <label>Wat is je favoriete instrument?</label>
+        <input type="text" name="instrument" />
+    </p>
+    
+    <p>
+        <label>&nbsp;</label>
+        <input onClick=(./mood.php) type="submit" value="Versturen" />
+    </p>
+  </form>
+</body>
+</html>
